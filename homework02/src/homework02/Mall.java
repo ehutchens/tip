36c3a369fc8721ab4243d440eb230ab3b5706587
ehutchens <ehutchens@carroll.edu>
2016-02-18 19:05:35 -0700

    Almost finished, missing a few things

--- homework02/src/homework02/Mall.java
+++ homework02/src/homework02/Mall.java
@@ -2,16 +2,17 @@ package homework02;
 
 public class Mall extends MallCentral {
 
+	private static final String allMalls = "Malls in America";
 	private String mallName;
 	private String location;
 	private int nos;
-	private double rating;
+	private int rating;
 	
 	public Mall() {
 		mallName = "SouthCenter";
 		location = "Tukwila, Washington";
 		nos = 120;
-		rating = 4.5;
+		rating = 4;
 	}
 	
 	@Override 
--- homework02/src/homework02/MallCentral.java
+++ homework02/src/homework02/MallCentral.java
@@ -1,25 +1,31 @@
 package homework02;
 
+import java.util.Scanner;
+
 public class MallCentral {
 
 	private static final String allMalls = "Malls in America";
 	private String mallName;
 	private String location;
 	private int nos;
-	private double rating;
+	private int rating;
 	
-	public MallCentral (){
+	public static Scanner mallScanner = new Scanner(System.in);
+	
+	
+	public MallCentral() {
+		mallName = "Mall Name";
+		location = "Location";
+		nos = 0;
+		rating = 0;
 		
-	 mallName = "unknown";
-	 mallName = "unknown";
-	 nos = 0;
-	 rating = 0.0;
 	}
 	
-	public MallCentral(String mName, String c) {
-		allMalls = "SouthCenter";
-		mallName = "mname";
-		location = 
+	public MallCentral(String nm, String l) {
+		mallName = nm;
+		location = l;
+		nos = 0;
+		rating = 0;
 		
 	}
 	
@@ -36,11 +42,44 @@ public class MallCentral {
 	}
 	
 	public int getnos() {
-		return (nos);
+		return(nos);
 	}
 	
-	public double getrating() {
-		return (rating);
+	public int getrating() {
+		return(rating);
 	}
 	
+	public void getnos(int n) {
+		nos = n;
+		return;
+	}
+	
+	public void getrating(int r) {
+		rating = r;
+		return;
+	}
+	
+	public String setmallName() {
+		System.out.println("Enter your Malls Name: ");
+		mallName = mallScanner.next();
+		return mallName;
+	}
+	
+	public String setlocation() {
+		System.out.println("Enter your Malls Location: ");
+		mallName = mallScanner.next();
+		return location;
+	}
+	
+	public int setnos() {
+		System.out.println("Enter the Number of Stores in " + mallName);
+		nos = mallScanner.nextInt();
+		return nos;
+	}
+	
+	public int setrating() {
+		System.out.println("Mall rating from 0 to 5: ");
+		rating = mallScanner.nextInt();
+		return rating;
+	}
 }
